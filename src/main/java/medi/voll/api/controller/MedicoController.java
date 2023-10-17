49ede8a17932121e261daf63c3e1ccc435cd2656
pedro freire <pedrofc31.31@gmail.com>
2023-10-16 21:35:22 -0300

    Adding Repository and Entities

--- src/main/java/medi/voll/api/controller/MedicoController.java
+++ src/main/java/medi/voll/api/controller/MedicoController.java
@@ -1,6 +1,9 @@
 package medi.voll.api.controller;
 
 import medi.voll.api.medico.DadosCadastroMedico;
+import medi.voll.api.medico.Medico;
+import medi.voll.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("medicos")
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository repository;
+
     @PostMapping
     public void cadastrar(@RequestBody DadosCadastroMedico dados) {
-        System.out.println(dados.endereco().cidade());
+        repository.save(new Medico(dados));
     }
 
 
--- src/main/java/medi/voll/api/endereco/Endereco.java
+++ src/main/java/medi/voll/api/endereco/Endereco.java
@@ -18,4 +18,14 @@ public class Endereco {
     private String uf;
     private String complemento;
     private String numero;
+
+    public Endereco(DadosEndereco endereco) {
+        this.logradouro = endereco.logradouro();
+        this.bairro = endereco.bairro();
+        this.cep = endereco.cep();
+        this.cidade = endereco.cidade();
+        this.uf = endereco.uf();
+        this.complemento = endereco.complemento();
+        this.numero = endereco.numero();
+    }
 }
